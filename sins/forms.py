2f871a30be5a83e65d0db86c76ae7a2080006273
Amy Woodehy <amywoodehy@gmail.com>
2016-11-23 01:00:13 +0600

    ReCaptcha in forms

--- sins/forms.py
+++ sins/forms.py
@@ -1,9 +1,18 @@
-from django.forms import Form
+from django.forms import Form, ModelForm
+from captcha.fields import ReCaptchaField
+
+from sins.models import Sinner, Sin
 
 
 class VoteForCost(Form):
     pass
 
 
-class AboutQuestionnaire(Form):
-    pass
+class CreateYourSinForm(ModelForm):
+    model = Sin
+    captcha = ReCaptchaField()
+
+
+class AboutQuestionnaire(ModelForm):
+    model = Sinner
+    captcha = ReCaptchaField()
