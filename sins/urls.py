93a5912a50f500f6e991778c56ac72b5f598bb49
Amy Woodehy <amywoodehy@gmail.com>
2016-11-23 00:58:51 +0600

    Little fix on url id to pk

--- sins/urls.py
+++ sins/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.IndexPageView.as_view(), name='index-page'),
     url(r'^m/$', views.SinsListMale.as_view(), name='male-sin-list'),
     url(r'^f/$', views.SinsListFemale.as_view(), name='female-sin-list'),
-    url(r'^s/(?P<id>\d+)/$', views.SinDetailView.as_view(), name='sin-detail'),
+    url(r'^s/(?P<pk>\d+)/$', views.SinDetailView.as_view(), name='sin-detail'),
     url(r'^create/$', views.SinCreateView.as_view(), name='sin-create')
 ]
